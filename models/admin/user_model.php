85999967f86b250d128d08b8aadc49a09d73a5a0
Adam Griffiths <adamgriffiths@zenbe.com>
2009-04-27 15:10:50 +0100

    Added models

---
+++ models/admin/user_model.php
@@ -0,0 +1,30 @@
+<?php
+
+class User_model extends Model
+{
+
+    function User_model()
+    {
+        parent::Model();
+    }
+
+	function users()
+	{
+		$query = $this->db->query("SELECT * FROM `users` ORDER BY `id` ASC");
+		return $query->result_array();
+	}
+	
+	function delete($id)
+	{
+		$this->db->query("DELETE FROM `users` WHERE `id` = '$id'");
+	}
+	
+	function edit($id)
+	{
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]|max_length[40]|callback_username_check');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[12]');
+		$this->form_validation->set_rules('remember', 'Remember Me');
+	}
+}
+
+?>
