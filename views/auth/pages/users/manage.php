c227e5cba02060ac10c960426aa5e3a1e6c9f73d
Adam Griffiths <adamgriffiths@zenbe.com>
2009-05-16 19:24:28 +0100

    Fixed incorrect admin URL when adding users

--- views/auth/pages/users/manage.php
+++ views/auth/pages/users/manage.php
@@ -1,5 +1,5 @@
 <h2>Manage Users</h2>
 
-<h3><?php echo anchor("users/add", "Add User"); ?></h3>
+<h3><?php echo anchor("admin/users/add", "Add User"); ?></h3>
 
 <?php echo $this->table->generate(); ?>
