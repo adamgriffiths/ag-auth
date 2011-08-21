8399ec29f01c91bfe7fb60208db64c94bf559e4c
Adam Griffiths <adam@adamgriffiths.co.uk>
2011-08-21 21:13:05 +0100

    Adding in & making v2.0.0 changes.

---
+++ core/MY_Controller.php
@@ -0,0 +1,135 @@
+<?php
+/**
+* Authentication Library
+*
+* @package Authentication
+* @category Libraries
+* @author Adam Griffiths
+* @link http://adamgriffiths.co.uk
+* @version 2.0.0
+* @copyright Adam Griffiths 2010
+*
+* Auth provides a powerful, lightweight and simple interface for user authentication .
+*/
+
+class Application extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		
+		log_message('debug', 'Application Loaded');
+
+		$this->load->library('form_validation');
+		$this->load->library('aauth');
+		$this->load->helper('url');
+	}
+	
+	function field_exists($value)
+	{
+		$field_name  = (valid_email($value)  ? 'email' : 'username');
+		
+		$query = $this->db->get_where('users', array($field_name => $value));
+		
+		if($query->num_rows() <> 0)
+		{
+			$this->form_validation->set_message('field_exists', 'The ' . $field_name . ' provided already exists, please use another.');
+			
+			return FALSE;
+		}
+		else
+		{			
+			return TRUE;
+			
+		} // if($this->field_exists($value) === TRUE)
+		
+	} // function field_exists($value)
+	
+	function register()
+	{
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[6]|callback_field_exists');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[6]|matches[password_conf]');
+		$this->form_validation->set_rules('password_conf', 'Password Confirmation', 'required|min_length[6]|matches[password]');
+		$this->form_validation->set_rules('email', 'Email Address', 'required|min_length[6]|valid_email|callback_field_exists');
+
+		if($this->form_validation->run() == FALSE)
+		{
+			$this->ag_auth->view('register');
+		}
+		else
+		{
+			$username = set_value('username');
+			$password = $this->ag_auth->salt(set_value('password'));
+			$email = set_value('email');
+
+			if($this->ag_auth->register($username, $password, $email) === TRUE)
+			{
+				$data['message'] = "The user account has now been created.";
+				$this->ag_auth->view('message', $data);
+				
+			} // if($this->ag_auth->register($username, $password, $email) === TRUE)
+			else
+			{
+				$data['message'] = "The user account has not been created.";
+				$this->ag_auth->view('message', $data);
+			}
+			
+			echo('7');
+		} // if($this->form_validation->run() == FALSE)
+		
+	} // function register()
+	
+	
+	function login($redirect = NULL)
+	{
+		
+		if($redirect === NULL)
+		{
+			$redirect = $this->ag_auth->config['auth_login'];
+		}
+		
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[6]');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
+		
+		if($this->form_validation->run() == FALSE)
+		{
+			$this->ag_auth->view('login');
+		}
+		else
+		{
+			$username = set_value('username');
+			$password = $this->ag_auth->salt(set_value('password'));
+			$field_type  = (valid_email($username)  ? 'email' : 'username');
+			
+			$user_data = $this->ag_auth->get_user($username, $field_type);
+			
+			
+			if($user_data['password'] === $password)
+			{
+				
+				unset($user_data['password']);
+				unset($user_data['id']);
+
+				$this->ag_auth->login_user($user_data);
+				
+				redirect($redirect);
+				
+				
+			} // if($user_data['password'] === $password)
+			else
+			{
+				$data['message'] = "The username and password did not match.";
+				$this->ag_auth->view('message', $data);
+			}
+		} // if($this->form_validation->run() == FALSE)
+		
+	} // login()
+	
+	function logout()
+	{
+		$this->session->sess_destroy();
+	}
+}
+
+/* End of file: MY_Controller.php */
+/* Location: application/core/MY_Controller.php */
--- libraries/Auth.php
+++ libraries/Auth.php
@@ -1,59 +1,58 @@
 <?php
-ob_start();
 /**
-* The Authentication Library
+* Authentication Library
 *
 * @package Authentication
 * @category Libraries
 * @author Adam Griffiths
 * @link http://adamgriffiths.co.uk
-* @version 1.0.6
-* @copyright Adam Griffiths 2009
+* @version 2.0.0
+* @copyright Adam Griffiths 2010
 *
-* Auth provides a powerful, lightweight and simple interface for user authentication 
+* Auth provides a powerful, lightweight and simple interface for user authentication .
 */
+
+ob_start();
 
 class Auth
 {
-	
 	var $CI; // The CI object
 	var $config; // The config items
-	var $user_table; // The user table (prefix + config)
-	var $group_table; // The group table (prefix + config)
 	
-	/** 
-	* Auth constructor
+	
+	/**
+	* @author Adam Griffiths
+	* @param array
 	*
-	* @access public
-	* @param string
+	* The constructor function loads the libraries dependancies and creates the 
+	* login attempts cookie if it does not already exist.
 	*/
-	function Auth($config)
+	function __construct($config)
 	{
-		$this->CI =& get_instance();
+		log_message('debug', 'Auth Library Loaded');
+
 		$this->config = $config;
-		
+
+		$this->CI =& get_instance();
+
 		$this->CI->load->database();
-		$this->CI->load->helper(array('form', 'url', 'email'));
-		$this->CI->load->library('form_validation');
 		$this->CI->load->library('session');
-		
+		$this->CI->load->helper('email');
+
+		$this->CI->load->model('auth_model');
+
 		$this->CI->lang->load('auth', 'english');
 		
-		$this->user_table = $this->CI->db->dbprefix($this->config['auth_user_table']);
-		$this->group_table = $this->CI->db->dbprefix($this->config['auth_group_table']);
-		
-		if($this->logged_in())
-		{
-			$this->_verify_cookie();
-		}
-		else
+		if($this->logged_in() == FALSE)
 		{
 			if(!array_key_exists('login_attempts', $_COOKIE))
 			{
 				setcookie("login_attempts", 0, time()+900, '/');
 			}
 		}
-	} // function Auth()
+		
+	}
+	
 	
 	/** 
 	* Restricts access to a page
@@ -81,7 +80,7 @@ class Auth
 		elseif($this->logged_in() == TRUE)
 		{
 			$level = $this->config['auth_groups'][$group];
-			$user_level = $this->CI->session->userdata('group_id');
+			$user_level = $this->CI->session->userdata('group');
 			
 			if($user_level > $level OR $single == TRUE && $user_level !== $level)
 			{
@@ -97,183 +96,15 @@ class Auth
 	} // function restrict()
 	
 	
-	/** 
-	* Log a user in
+	/**
+	* @author Adam Griffiths
+	* @return bool
 	*
-	* Log a user in a redirect them to a page specified in the $redirect variable
-	*
-	* @access public
-	* @param string
-	*/
-	function login($redirect = NULL)
-	{
-		if($redirect === NULL)
-		{
-			$redirect = $this->config['auth_login'];
-		}
-
-			
-		$this->CI->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]|max_length[40]|callback_username_check');
-		$this->CI->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[12]');
-		$this->CI->form_validation->set_rules('remember', 'Remember Me');
-
-		if($this->CI->form_validation->run() == FALSE)
-		{
-			if((array_key_exists('login_attempts', $_COOKIE)) && ($_COOKIE['login_attempts'] >= 5))
-			{
-				echo $this->CI->lang->line('max_login_attempts_error');
-			}
-			else
-			{
-				$this->view('login');
-			}
-		}
-		else
-		{
-			$username = set_value('username');
-			$auth_type = $this->_auth_type($username);
-			$password = $this->_salt(set_value('password'));
-			$email = set_value('email');
-
-			if(!$this->_verify_details($auth_type, $username, $password))
-			{
-				show_error($this->CI->lang->line('login_details_error'));
-				$redirect = $this->config['auth_incorrect_login'];
-				redirect($redirect);
-			}
-
-			$userdata = $this->CI->db->query("SELECT * FROM `$this->user_table` WHERE `$auth_type` = '$username'");
-			$row = $userdata->row_array();
-			
-			$data = array(
-						$auth_type => $username,
-						'username' => $row['username'],
-						'user_id' => $row['id'],
-						'group_id' => $row['group_id'],
-						'logged_in' => TRUE
-						);
-			$this->CI->session->set_userdata($data);
-
-			if($this->config['auth_remember'] === TRUE)
-			{
-				$this->_generate();
-			}
-
-			redirect($redirect);
-		}
-	} // function login()
-	
-	
-	/** 
-	* Logout - logs a user out
-	*
-	* @access public
-	*/
-	function logout($redirect = NULL)
-	{
-		if($redirect === NULL)
-		{
-			$redirect = $this->config['auth_logout'];
-		}
-
-		$this->CI->session->sess_destroy();
-		redirect($redirect);
-	} // function logout()
-	
-	
-	/** 
-	* Register a new user
-	*
-	* Register a user and redirect them to the success page
-	*
-	* @access public
-	* @param bool whether or not the user should be logged in once account is created, used in admin panel
-	* @param bool whether or not the user is simply being edited, used in admin panel
-	* @param string the user ID to be edited, used in the admin panel
-	*/
-	function register($login = TRUE, $edit = FALSE, $id = NULL)
-	{
-		if($edit === TRUE)
-		{
-			$this->CI->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|callback_reg_email_check');
-		}
-		else
-		{
-			$this->CI->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]|max_length[40]|callback_reg_username_check');
-			$this->CI->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[12]|matches[conf_password]');
-			$this->CI->form_validation->set_rules('conf_password', 'Password confirmation', 'trim|required|min_length[4]|max_length[12]|matches[password]');
-			$this->CI->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|callback_reg_email_check');
-		}
-		
-		if($this->CI->form_validation->run() == FALSE)
-		{
-			if($edit === TRUE)
-			{
-				$query = $this->CI->db->query("SELECT * FROM `$this->user_table` WHERE `id` = '$id'");
-				$result = $query->result_array();
-				
-				$this->view('register', $result[0]);
-			}
-			else
-			{
-				$this->view('register');
-			}
-		}
-		else
-		{
-			
-			$username = set_value('username');
-			$password = $this->_salt(set_value('password'));
-			$email = set_value('email');
-			
-			if($edit === TRUE)
-			{
-				$this->CI->db->query("UPDATE `$this->user_table` SET `email` = '$email' WHERE `id` = '$id'");
-				$data2['msg'] = "The user has now been edited.";
-			}
-			else
-			{
-				$this->CI->db->query("INSERT INTO `$this->user_table` (username, email, password) VALUES ('$username', '$email', '$password')");
-				$data2['msg'] = "The user has now been created.";
-			}
-			
-			if($login === TRUE)
-			{
-				$data2['msg'] = "The user has been created, you have now been logged in.";
-				
-				$userdata = $this->CI->db->query("SELECT * FROM `$this->user_table` WHERE `username` = '$username'");
-				$row = $userdata->row_array();
-			
-				$data = array(
-						'username' => $username,
-						'user_id' => $row['id'],
-						'group_id' => $row['group_id'],
-						'logged_in' => TRUE
-						);
-				$this->CI->session->set_userdata($data);
-			
-				if($this->config['auth_remember'] === TRUE)
-				{
-					$this->_generate();
-				}
-			}
-			
-			$this->view('reg_success', $data2);
-		}
-	} // function register()
-
-	
-	/** 
-	* Check to see if a user is logged in
-	*
-	* Look in the session and return the 'logged_in' part
-	*
-	* @access public
-	* @param string
+	* Checks the session data as to whether or not a user is logged in.
 	*/
 	function logged_in()
 	{
-		if($this->CI->session->userdata('logged_in') == TRUE)
+		if($this->CI->session->userdata('logged_in') === TRUE)
 		{
 			return TRUE;
 		}
@@ -281,78 +112,100 @@ class Auth
 		{
 			return FALSE;
 		}
-	} // function logged_in()
+	}
+	
+	
+	/**
+	* @author Adam Griffiths
+	* @param string
+	* @return string
+	*
+	* Uses the encryption key set in application/config/config.php to salt the password passed.
+	*/
+	function salt($password)
+	{
+		return hash("haval256,5", $this->CI->config->item('encryption_key') . $password);
+	}
+	
+	
+	/**
+	* @author Adam Griffiths
+	* @param string
+	* @param string
+	* @return string / bool
+	*
+	* Takes a username & optional username type (email/username) and returns the user data
+	*/
+	function get_user($username, $field_type = 'username')
+	{
+		$user = $this->CI->auth_model->login_check($username, $field_type);
 
-
-	/** 
-	* Check to see if a user is logging in with their username or their email
-	*
-	* @access private
+		return $user;
+	}
+	
+	
+	/**
+	* @author Adam Griffiths
 	* @param string
-	*/
-	function _auth_type($str)
-	{
-		if(valid_email($str))
-		{
-			return 'email';
-		}
-		else
-		{
-			return 'username';
-		}
-	} // function _auth_type()
-	
-	
-	/** 
-	* Salt the users password
-	*
-	* @access private
 	* @param string
-	*/
-	function _salt($str)
-	{
-		return sha1($this->CI->config->item('encryption_key').$str);
-	} // function _salt()
-	
-	
-	/** 
-	* Verify that their username/email and password is correct
-	*
-	* @access private
 	* @param string
+	* @return bool
+	*
+	* Creates a new user account
 	*/
-	function _verify_details($auth_type, $username, $password)
+	function register($username, $password, $email)
 	{
-		$query = $this->CI->db->query("SELECT * FROM `$this->user_table` WHERE `$auth_type` = '$username' AND `password` = '$password'");
+		return $this->CI->auth_model->register($username, $password, $email);
+	}
+	
+	
+	/**
+	* @author Adam Griffiths
+	* @param array
+	*
+	* Takes the user array, adds the logged_in portion and sets the session data from that.
+	*/
+	function login_user($user)
+	{
+		$user['logged_in'] = TRUE;
 		
-		if($query->num_rows != 1)
-		{
-			$attempts = $_COOKIE['login_attempts'] + 1;
-			setcookie("login_attempts", $attempts, time()+900, '/');
-			return FALSE;
-		}
-		
-		return TRUE;
-	} // function _verify_details()
+		$this->CI->session->set_userdata($user);
+	}
 	
 	
 	/** 
-	  * Generate a new token/identifier
-	  *
-	  * @access private
-	  * @param string
-	  */
-	  function _generate()
-	  {
-	    $username = $this->CI->session->userdata('username');
+	* Generate a new token/identifier from random.org
+	*
+	* @author Adam Griffiths
+	* @access private
+	* @param string
+	*/
+	private function _generate()
+	{
+	  $username = $this->CI->session->userdata('username');
 
-        // Generate a random string ourselves
-        $length = 20;
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $token = ”;    
-        for ($i = 0; $i < $length; $i++) {
-            $token .= $characters[mt_rand(0, strlen($characters)-1)];
-        }
+	  $rand_url = 'http://random.org/strings/?num=1&len=20&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new';
+
+	  if (ini_get('allow_url_fopen')) {
+	    // Grab the random string using the easy version if we can
+	    $token_source = fopen($rand_url, "r");
+	    $token = fread($token_source, 20);
+	  } elseif (function_exists('curl_version')) {
+	    // No easy version, so try cURL
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL, $rand_url);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    $token = curl_exec($ch);
+	    curl_close($ch);
+	  } else {
+	    // No love either way, generate a random string ourselves
+	    $length = 20;
+	      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	      $token = ”;    
+	      for ($i = 0; $i < $length; $i++) {
+	          $token .= $characters[mt_rand(0, strlen($characters)-1)];
+	      }
+	  }
 
 	    $identifier = $username . $token;
 	    $identifier = $this->_salt($identifier);
@@ -369,9 +222,9 @@ class Auth
 	* @access private
 	* @param string
 	*/
-	function _verify_cookie()
+	private function _verify_cookie()
 	{
-		if((array_key_exists('login_attempts', $_COOKIE)) && ($_COOKIE['login_attempts'] >= 5))
+		if((array_key_exists('login_attempts', $_COOKIE)) && ($_COOKIE['login_attempts'] <= 5))
 		{
 			$username = $this->CI->session->userdata('username');
 			$userdata = $this->CI->db->query("SELECT * FROM `$this->user_table` WHERE `username` = '$username'");
@@ -394,6 +247,7 @@ class Auth
 		}
 	}
 	
+	
 	/** 
 	* Load an auth specific view
 	*
@@ -410,6 +264,9 @@ class Auth
 		$data['page'] = $page;
 		$this->CI->load->view($this->config['auth_views_root'].'index', $data);
 	}
-} // class Auth
+	
 
-?>
+}
+
+/* End of file: Auth.php */
+/* Location: application/libraries/Auth.php */
